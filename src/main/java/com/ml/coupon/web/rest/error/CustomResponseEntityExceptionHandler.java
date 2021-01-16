981a9606b2b9e930664350c727d7a8773f3bc1ae
LIEBENER Leonardo <c.lliebener@ternium.com.ar>
2021-01-16 20:06:19 -0300

    Api First approach
    - Api first endpoints
    - Refactoring validations error

--- src/main/java/com/ml/coupon/web/rest/error/CustomResponseEntityExceptionHandler.java
+++ src/main/java/com/ml/coupon/web/rest/error/CustomResponseEntityExceptionHandler.java
@@ -3,6 +3,7 @@ package com.ml.coupon.web.rest.error;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -17,6 +18,15 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
             HttpStatus status, WebRequest request) {
 
         String error = ex.getParameterName() + " parameter is missing";
+        ApiError apiError =
+                new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), error);
+        return new ResponseEntity<Object>(
+                apiError, new HttpHeaders(), apiError.getStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String error = ex.getParameter() + " parameter is missing";
         ApiError apiError =
                 new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), error);
         return new ResponseEntity<Object>(
